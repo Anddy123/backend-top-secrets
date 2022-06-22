85afb0a4bc1cf7d2128df0aa331b087b26a265f9
Anddy <alonzoa2015@gmail.com>
2022-06-22 15:37:59 -0700

    setup user tests

---
+++ __tests__/users.test.js
@@ -0,0 +1,76 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('users can signup with email and password with POST', async() => {
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send({ 
+        email: 'bob@bob.com', 
+        password: 'bobbob' 
+      });
+
+    expect(res.body).toEqual({
+      id: expect.any(String), 
+      email: 'bob@bob.com'
+    });
+  });
+
+  it('signs in an existing user', async() => {
+    //first we sign up a user
+    const user = await UserService.hash({
+      email: 'bob@bob.com', 
+      password: 'bobbob'
+    });
+
+    //then we test signing in the user
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({
+        email: 'bob@bob.com',
+        password: 'bobbob'
+      });
+
+    expect(res.body).toEqual({
+      message: 'Signed in successfully',
+      user,
+    });
+  });
+
+
+  it('logs out a user', async() => {
+
+    await UserService.hash({
+      email: 'bob2@bob.com', 
+      password: 'bobbob'
+    });
+
+
+    await request(app)
+      .post('/api/v1/users/sessions')
+      .send({
+        email: 'bob2@bob.com',
+        password: 'bobbob'
+      });
+
+    const res = await request(app)
+      .delete('/api/v1/users/sessions');
+    
+
+    expect(res.body).toEqual({
+      success: true, 
+      message: 'Signed out successfully!'
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
